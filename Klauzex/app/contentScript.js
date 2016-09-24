fd135ebea905552f53b821c52fb4c2ede330ecb0
sasza8 <krzysztof@stopczanski.pl>
2016-09-24 14:03:45 +0200

    added finding links with policies, added jquery

--- Klauzex/app/contentScript.js
+++ Klauzex/app/contentScript.js
@@ -1,4 +1,24 @@
 ﻿chrome.storage.local.get('dictionary', function (dictionary) {
-    dictionary  = dictionary.dictionary;
-    console.log(dictionary);
+    dictionary = dictionary.dictionary;
+    inputContent = document.all[0].innerText;
+    findLinksWithPolicies();
 });
+
+function findLinksWithPolicies()
+{
+    var policiesLinks = [];
+    var policiesTerms = ["regulamin", "zasady", "warunki", "reguły"];
+
+    $('a').each(function() {
+        for (i = 0; i < policiesTerms.length; i++)
+        {
+            if (this.innerHTML.indexOf(policiesTerms[i]) != -1)
+            {
+                policiesLinks.push(this);
+                break;
+            }
+        }
+    });
+    
+    return policiesLinks;
+}

10a61eb9cfee10c3c0b15bd4344dd1786ace7a67
julek <julek@KROWA>
2016-09-24 15:36:43 +0200

    zmiana interfejsu

--- Klauzex/app/algosy.js
+++ Klauzex/app/algosy.js
@@ -9,7 +9,7 @@
         } else {
             gotAnswer([
                 {
-                    clause: clauses[0],
+                    clause: clauses[0].id,
                     text: plaintext[0]
                 }
             ]);
@@ -17,19 +17,6 @@
     });
 }
 
-//chrome.runtime.sendMessage({ request: "clauses" }, function (response) {
-//    clauses = response.clauses;
-//    plaintext = plaintext.split(" ").filter(function (text) {
-//        return text != "";
-//    });
-//    if (plaintext.length == 0) {
-//        gotAnswer([]);
-//    } else {
-//        gotAnswer([
-//            {
-//                clause: clauses[0],
-//                text: plaintext[0]
-//            }
-//        ]);
-//    }
+//chrome.runtime.sendMessage({ request: "changeHtml" }, function (response) {
+//    document.body.innerHTML = response.html;
 //});

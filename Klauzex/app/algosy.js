716e80fe472da3d9023bbc2879211e682e38bc64
julek <julek@KROWA>
2016-09-24 13:30:02 +0200

    Smieci

--- Klauzex/app/algosy.js
+++ Klauzex/app/algosy.js
@@ -1,1 +1,11 @@
-﻿
+﻿Clauses.get('https://www.uokik.gov.pl/download.php?id=1064').then(function (clauses) {
+    //console.log(clauses);
+    console.log(clauses[0].branch);
+    var filtered = clauses.filter(function (clause) {
+        if(clause.branch){
+            return clause.branch.indexOf("ELEKTRONICZNY") != -1
+        }
+        return false;
+    });
+    console.log(filtered);
+});

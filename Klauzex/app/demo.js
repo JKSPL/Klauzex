57194b0d992a402aeb82cbd96c8228823c25fafa
Michal Papierowski <michal.papierowski@gmail.com>
2016-09-25 08:36:34 +0200

    Removed button from demo view

--- Klauzex/app/demo.js
+++ Klauzex/app/demo.js
@@ -1,10 +1,9 @@
 ﻿$(document).ready(function()
 {
 	var placeholder = "Wpisz tekst aby zobaczyć jego reprezentację po unifikacji...";
-    $('#process').click(function ()
-    {
-    	var inputText = $('#input_string').val();
+	$('#input_string').keyup(function() {
+		var inputText = $('#input_string').val();
     	var newText = inputText != '' ? sanitizeSingle(inputText) : placeholder;
         $('#output').text(newText);
-    });
+	});
 });
